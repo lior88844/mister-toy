2b471e06340780d38aed567055bce4cc962a553c
lior88844 <lior88844@gmail.com>
2023-03-14 13:05:00 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const toyService = require('./services/toy.service')
+const cors = require('cors')
+const app = express()
+
+const port = process.env.PORT || 3038
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true,
+}
+
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(express.static('public'))
+
+// LIST
+app.get('/api/toy', (req, res) => {
+  const criteria = req.query
+
+  toyService
+    .query(criteria)
+    .then(toys => res.send(toys))
+    .catch(err => res.status(500).send('Cannot get toys'))
+})
+// READ
+app.get('/api/toy/:toyId', (req, res) => {
+  const { toyId } = req.params
+
+  toyService
+    .getById(toyId)
+    .then(toy => res.send(toy))
+    .catch(err => res.status(500).send('Cannot get toy'))
+})
+//DELETE
+app.delete('/api/toy/:toyId', (req, res) => {
+  const { toyId } = req.params
+
+  toyService
+    .remove(toyId)
+    .then(() => res.send('Removed!'))
+    .catch(err => res.status(401).send(err))
+})
+// CREATE
+app.post('/api/toy', (req, res) => {
+  const toy = req.body
+  toyService
+    .save(toy)
+    .then(savedtoy => res.send(savedtoy))
+    .catch(err => res.status(500).send('Cannot save toy'))
+})
+
+// UPDATE
+app.put('/api/toy/:toyId', (req, res) => {
+  const toy = req.body
+  toyService
+    .save(toy)
+    .then(savedtoy => {
+      console.log(savedtoy)
+      res.send(savedtoy)
+    })
+    .catch(err => res.status(500).send('Cannot save toy'))
+})
+
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
---
+++ services/toy.service.js
@@ -0,0 +1,100 @@
+const fs = require('fs')
+const Toys = require('../data/Toy.json')
+
+const PAGE_SIZE = 3
+
+module.exports = {
+  query,
+  save,
+  remove,
+  getById,
+}
+
+function query(criteria) {
+  const { filterBy, sortBy } = criteria
+  console.log(filterBy, sortBy);
+  let filteredToys = Toys
+  let filter = {
+    name: filterBy?.name || '',
+    isOnlyInStock: filterBy?.inStock === 'false' ? false : true || false,
+    labels: filterBy?.labels || [],
+    sortBy: filterBy?.sortBy || 'created',
+    page: filterBy?.page || 0,
+  }
+
+  let regex = new RegExp(filter.name, 'i')
+  filteredToys = filteredToys.filter(
+    toy =>
+      regex.test(toy.name) &&
+      (!filter.isOnlyInStock || toy.inStock === filter.isOnlyInStock) &&
+      (!filter.labels.length || filter.labels.some(label => toy.labels.includes(label)))
+  )
+
+  if (filter.page) {
+    const startIdx = filter.page * PAGE_SIZE
+    filteredToys = filteredToys.slice(startIdx, startIdx + PAGE_SIZE)
+  }
+
+  if (filter === 'name') {
+    filteredToys = filteredToys.sort((a, b) => a.name.localeCompare(b.name))
+  } else {
+    filteredToys = filteredToys.sort((a, b) => b[criteria.sortBy] - a[criteria.sortBy])
+  }
+
+  sortBy.by === 'name'
+    ? filteredToys.sort((toy1, toy2) => toy1[sortBy.by].localeCompare(toy2[sortBy.by]))
+    : filteredToys.sort((toy1, toy2) => { return (toy1[sortBy.by] - toy2[sortBy.by]) * sortBy.desc })
+
+  return Promise.resolve(filteredToys)
+}
+
+function save(toy) {
+  console.log(toy)
+  if (toy._id) {
+    const idx = Toys.findIndex(currToy => currToy._id === toy._id)
+    if (idx === -1) return Promise.reject('No such Toy')
+    Toys[idx] = toy
+  } else {
+    toy._id = _makeId()
+    toy.createdAt = Date.now()
+
+    Toys.push(toy)
+  }
+
+  return _saveToysToFile().then(() => toy)
+}
+
+function getById(ToyId) {
+  const Toy = Toys.find(Toy => Toy._id === ToyId)
+  return Promise.resolve(Toy)
+}
+
+function remove(ToyId) {
+  const idx = Toys.findIndex(Toy => Toy._id === ToyId)
+  if (idx === -1) return Promise.reject('No such Toy')
+
+  Toys.splice(idx, 1)
+  return _saveToysToFile()
+}
+
+function _makeId(length = 5) {
+  let txt = ''
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
+}
+
+function _saveToysToFile() {
+  return new Promise((resolve, reject) => {
+    const content = JSON.stringify(Toys, null, 2)
+    fs.writeFile('./data/Toy.json', content, err => {
+      if (err) {
+        console.error(err)
+        return reject(err)
+      }
+      resolve()
+    })
+  })
+}
